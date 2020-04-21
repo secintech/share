e318064d485f5aeba3ea2af2f37d15e346ae90cf
Samuel <ferraxi@gmail.com>
2020-04-21 16:56:37 +0200

    main

--- kotlin/stairs/main.kt
+++ kotlin/stairs/main.kt
@@ -1,24 +1,17 @@
 import java.nio.ByteBuffer
 
-fun generateDNA(size: Int = 1000000, consume: (b: Byte)->Unit){
-    var idx =0
-    for(i in 0..size) {
-        consume(idx.toByte())
-        idx = (idx + 1)%4
-    }
-}
+const val SIZE = 100
+
+var vsize = 0
+val stairs = Stairs(100)
+val buffer = arrayListOf<Byte>()
 
 fun main(){
-    val SIZE = 100
-    var vsize = 0
-    val stairs = Stairs(100)
-    val buffer = arrayListOf<Byte>()
     generateDNA(){
         buffer.add(it)
         if (buffer.size == SIZE) {
             stairs.add(ByteBuffer.wrap(buffer.toByteArray()), 1) {
                 printStairs(it)
-                vsize += it.size
             }
             buffer.clear()
         }
@@ -27,7 +20,17 @@ fun main(){
    println("\nvector size: $vsize")
 }
 
+fun generateDNA(size: Int = 10000000, consume: (b: Byte)->Unit){
+    var idx =0
+    for(i in 0..size) {
+        consume(idx.toByte())
+        idx = (idx + 1)%4
+    }
+}
+
+
 fun printStairs(v: Array<Float>) {
     v.forEach { print("$it ") }
+    vsize += v.size
 }
 
